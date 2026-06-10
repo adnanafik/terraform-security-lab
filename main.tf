e026ecb1ca3755f04ae2ae149baeecd0ca70ac96
Adnan Khan <adnanak@uci.edu>
2026-06-10 02:22:51 -0500

    test: enable public access - should fail checkov

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ resource "azurerm_storage_account" "app" {
   account_tier                  = "Standard"
   account_replication_type      = "LRS"
   min_tls_version               = "TLS1_2"
-  allow_nested_items_to_be_public = false
-  public_network_access_enabled = false
+  allow_nested_items_to_be_public = true
+  public_network_access_enabled = true
 
   tags = {
     environment = "lab"
